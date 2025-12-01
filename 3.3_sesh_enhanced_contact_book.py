ad997aa1e8b9a7a76e3e92a272daadf5b3e606d0
Krsvzn <krish.04062613@gmail.com>
2025-12-01 23:48:03 +0530

    Create 3.3_sesh_enhanced_contact_book.py
    the same contact book with error handling.

---
+++ 3.3_sesh_enhanced_contact_book.py
@@ -0,0 +1,28 @@
+phonebook = {}
+
+while True:
+ try:
+    choice = int(input("____________________________________\n<<<<Welcome To The Contacts>>>>\n\n1. Add Contact -> \n\n2. Search Contact -> \n\n3. Show All Contacts -> \n\n4. Exit -> \n____________________________________\nEnter Your Choice -> "))   
+    if choice == 1:
+        name = input("\nEnter The Name -> ")
+        ph = int(input("\nEnter The Phone Number -> "))
+        phonebook[name] = ph
+        print("\n\n***Contact Saved Successfully***")
+    elif choice == 2:
+        f_name = input("\nEnter The Name Of The Contact To Search -> ")
+        if f_name in phonebook:
+         print("\n",phonebook[f_name])
+        else:
+           print("\nContact Not Found")
+    elif choice == 3:
+       print("***All Contacts In The Phone Book***\n",phonebook)
+       
+    elif choice == 4:
+       print("\nSuccessfully Exited\n____________________________________")
+       break
+    else:
+       print("\n:::::Sorry, Only Enter Values From (1-4)\nTry Again :D")
+ except ValueError:
+    print("\n:::::::Sorry, Only Integers Allowed As Input :D::::::::\nTry Again :D\n")  
+
+    
